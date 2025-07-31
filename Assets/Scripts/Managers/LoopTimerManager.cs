8204a5fdf28cf06efdaedccfafbf9296fb65fbf9
romanleco <94546246+romanleco@users.noreply.github.com>
2025-07-31 19:49:46 -0400

    Loop Timer
    The time loop is functional and displayed in the UI, there are no actions happening on time loop completion yet.

---
+++ Assets/Scripts/Managers/LoopTimerManager.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class LoopTimerManager : MonoSingleton<LoopTimerManager>
+{
+    public static event Action OnLoopComplete;
+    private const float LoopTimeS = 60;
+    private float _loopTimer;
+    private float _loopCompletionPercentage;
+
+    void Start()
+    {
+        _loopTimer = Time.time + LoopTimeS;
+    }
+    void Update()
+    {
+        if (_loopTimer <= Time.time)
+        {
+            _loopTimer = Time.time + LoopTimeS;
+            OnLoopComplete?.Invoke();
+        }
+
+        _loopCompletionPercentage = (Time.time - (_loopTimer - LoopTimeS)) / LoopTimeS;
+        UIManager.Singleton.UpdateLoopCompletion(_loopCompletionPercentage);
+        Debug.Log($"Loop Completion Percentage: {_loopCompletionPercentage}");
+    }
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 public class UIManager : MonoSingleton<UIManager>
 {
     [SerializeField] private TMP_Text[] _resourcesTexts = new TMP_Text[5];
+    [SerializeField] private Image _loopTimerImage;
     public void UpdateResourceCount(int resourceIndex, int value)
     {
         //0 = Wheat; 1 = Wood; 2 = Minerals; 3 = Units; 4 = Coins
         _resourcesTexts[resourceIndex].text = value.ToString();
     }
+
+    public void UpdateLoopCompletion(float completionPercentage) => _loopTimerImage.fillAmount = completionPercentage;
 }
